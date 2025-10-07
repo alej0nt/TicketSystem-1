5c24558927481b0306ceb738effd5b037f43d8e1
alej0nt <alej0nt666@gmail.com>
2025-10-07 17:46:23 -0500

    Puesto @RestControllerAdvice

--- backend/src/main/java/com/leoalelui/ticketsystem/domain/exception/GlobalHandlerException.java
+++ backend/src/main/java/com/leoalelui/ticketsystem/domain/exception/GlobalHandlerException.java
@@ -8,11 +8,12 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalHandlerException {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
