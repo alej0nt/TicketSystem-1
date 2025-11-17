d3ca515272f88e318349fa256fed8fcd6cb51450
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-11-17 00:10:38 -0500

    nueva url del backend en la nube

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseURL: "http://localhost:8080/api/v1",
-    //apiBaseURL: "https://ticketsystem-backend-tfyn.onrender.com/api/v1"
+    apiBaseURL: "https://ticketsystem-backend-tfyn.onrender.com/api/v1"
 }
